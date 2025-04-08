91446b6a3c9c4cb74c5f09dde6d057d19132364e
Dimaa411a <dimaandreiko8888@gmail.com>
2025-04-08 09:49:46 +0300

    5.3

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
-from .views import index
+from .views import HomePageView, ArticleDetail, ArticleList, ArticleCategoryList
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', HomePageView.as_view(), name='home'),
+    path('articles/', ArticleList.as_view(), name='articles-list'),
+    path('articles/category/<slug:slug>/', ArticleCategoryList.as_view(), name='articles-category-list'),
+    path('articles/<int:year>/<int:month>/<int:day>/<slug:slug>/', ArticleDetail.as_view(), name='news-detail'),
+]
+
+
+from django.urls import path
+from .views import HomePageView, ArticleDetail, ArticleList
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('articles/', ArticleList.as_view(), name='articles-list'),
+    path('articles/<int:year>/<int:month>/<int:day>/<slug:slug>/', ArticleDetail.as_view(), name='news-detail'),
 ]
